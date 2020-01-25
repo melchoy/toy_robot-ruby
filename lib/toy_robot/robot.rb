768e187c7043e65073a92d072d3704055cff3375
Melissa Choy <mel@melissachoy.com>
2020-01-25 18:22:52 +1100

    Initial Robot class with move and position methods

---
+++ lib/toy_robot/robot.rb
@@ -0,0 +1,17 @@
+module ToyRobot
+  class Robot 
+    
+    def initialize(position = 0)
+      @position = position
+    end
+
+    def move 
+      @position += 1     
+    end    
+
+    def position
+      @position
+    end
+
+  end
+end
