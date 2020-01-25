f104e799958c1b38a509e053faa33b3c4c1d50b1
Melissa Choy <mel@melissachoy.com>
2020-01-25 19:21:44 +1100

    Add functionality to move up and down

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,17 +1,26 @@
 module ToyRobot
   class Robot 
-    attr_reader :position
+    attr_reader :position_x, :position_y
     
-    def initialize(position = 0)
-      @position = position
+    def initialize(x = 0, y = 0)
+      @position_x = x
+      @position_y = y
     end
 
     def move_east 
-      @position += 1     
+      @position_x += 1     
     end 
     
     def move_west
-      @position -= 1
+      @position_x -= 1
+    end
+
+    def move_north
+      @position_y += 1
+    end
+    
+    def move_south
+      @position_y -= 1
     end
 
   end
--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -3,24 +3,44 @@ require "spec_helper"
 RSpec.describe ToyRobot::Robot do
   subject { ToyRobot::Robot.new(0) }
   
-  it "moves 3 spaces east" do
-    3.times { subject.move_east } 
-    expect(subject.position).to eq(3)
+  it "moves 1 spaces east" do
+    1.times { subject.move_east } 
+    expect(subject.position_x).to eq(1)
   end 
 
   it "moves 4 spaces east" do
-    4.times { subject.move_east } 
-   expect(subject.position).to eq(4)
+    3.times { subject.move_east } 
+   expect(subject.position_x).to eq(3)
+  end
+
+  it "moves 1 spaces west" do
+    1.times { subject.move_west }
+    expect(subject.position_x).to eq(-1)
   end
 
   it "moves 3 spaces west" do
     3.times { subject.move_west }
-    expect(subject.position).to eq(-3)
+    expect(subject.position_x).to eq(-3)
   end
 
-  it "moves 4 spaces west" do
-    4.times { subject.move_west }
-    expect(subject.position).to eq(-4)
+  it "moves 1 spaces north" do
+    1.times { subject.move_north }
+    expect(subject.position_y).to eq(1)
+  end
+
+  it "moves 3 spaces north" do
+    3.times { subject.move_north }
+    expect(subject.position_y).to eq(3)
+  end
+
+  it "moves 1 spaces south" do
+    1.times { subject.move_south }
+    expect(subject.position_y).to eq(-1)
+  end
+
+  it "moves 4 spaces south" do
+    3.times { subject.move_south }
+    expect(subject.position_y).to eq(-3)
   end
 
 end
